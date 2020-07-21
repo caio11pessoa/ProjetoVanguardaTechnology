a3b67dab4cf88fcca012a5730a7072e4c647cd08
Caio Pessoa <caiopessoa1298@gmail.com>
2020-07-21 19:57:21 -0300

    Adicionando a estrutura HTML5 do index.php, fazendo o esqueleto geral da tela

--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="CSS\estilo.css">
+    <title>Mapa</title>
 </head>
 <body>
+<header class="cabecalho">
+        <h1>VANGUARDA TECHNOLOGY</h1>
+        <h2>Mapa contendo os markers</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <div id="map"></div>
+        </div>
+    </main>
+    <footer class="rodape">
+        CAIO A. PESSOA
+    </footer>
     
 </body>
 </html>
