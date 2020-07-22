d1a285e6960fa6bcb5b58d9bde550ca0ca7e5af1
Caio Pessoa <caiopessoa1298@gmail.com>
2020-07-21 21:38:32 -0300

    Escrevendo o código de GET.php e recebendo a latitude e longitude dos markers no endpoint.

---
+++ Funcoes/GET.php
@@ -0,0 +1,7 @@
+<?php
+    $url = "https://middleware.vanguardatech.com/api/search/retorno_posicoes/";
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    $positionMarkers = json_decode(curl_exec($ch));
