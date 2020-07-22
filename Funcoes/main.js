461b97f6f50d60423514e04b9809dc0ab26b65d8
Caio Pessoa <caiopessoa1298@gmail.com>
2020-07-21 21:15:44 -0300

    coloando a função, em JavaScript, que ativa o mapa na tela

--- Funcoes/main.js
+++ Funcoes/main.js
@@ -8,38 +8,46 @@ const arrayButtons = [
         Label: document.getElementById('toggle1'),
         Button: document.getElementById('button1'),
         condicao: true
-   },
-    {
-        Toggle : document.getElementById('add'),
-        Label : document.getElementById('toggle2'),
-        Button : document.getElementById('button2'),
-        condica : true
     },
     {
-        Toggle : document.getElementById('clear'),
-        Label : document.getElementById('toggle3'),
-        Button : document.getElementById('button3'),
+        Toggle: document.getElementById('add'),
+        Label: document.getElementById('toggle2'),
+        Button: document.getElementById('button2'),
+        condica: true
+    },
+    {
+        Toggle: document.getElementById('clear'),
+        Label: document.getElementById('toggle3'),
+        Button: document.getElementById('button3'),
         condicao: true
 
     }
 ]
 
-function valida(validacao){
-    if(validacao.condicao){
+function valida(validacao) {
+    if (validacao.condicao) {
         validacao.condicao = false
-        validacao.Label.style.backgroundColor="#55d069"
+        validacao.Label.style.backgroundColor = "#55d069"
         validacao.Button.style.left = '25px'
-    }else{
+    } else {
         validacao.condicao = true
-        validacao.Label.style.backgroundColor="#900"
+        validacao.Label.style.backgroundColor = "#900"
         validacao.Button.style.left = '3px'
     }
-    
+
 }
 
-arrayButtons.forEach(element =>{
-    element.Toggle.addEventListener('click', ()=>{
+arrayButtons.forEach(element => {
+    element.Toggle.addEventListener('click', () => {
         valida(element)
     })
 })
+
+var map = L.map('map').setView([-3.7197137254680808, -38.529790341854095], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    id: "mapbox.streets"
+}).addTo(map);
 
