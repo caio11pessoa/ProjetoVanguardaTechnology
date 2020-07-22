42cc7472cf688b63d45ad7e8f26ecf187bc49b2b
Caio Pessoa <caiopessoa1298@gmail.com>
2020-07-22 09:57:32 -0300

    Refatorando parte do código e adicionando comentários

--- Funcoes/GET.php
+++ Funcoes/GET.php
@@ -5,3 +5,5 @@
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
     $positionMarkers = json_decode(curl_exec($ch));
+
+//aqui faço o método GET para receber os dados do endpoint
--- Funcoes/main.js
+++ Funcoes/main.js
@@ -1,3 +1,17 @@
+
+//Criando o mapa
+
+var map = L.map('map').setView([-3.7197137254680808, -38.529790341854095], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    id: "mapbox.streets"
+}).addTo(map);
+
+//recebe os ícones escritos no arquivo iconesMarker.js
+var icones = retornarIcon()
+
 /*Toggle: é o checkbox;
 Label: são as classes toggle1 a toogle3;
 button: são os botões que ficam da esquerda para direita*/
@@ -20,7 +34,6 @@ const arrayButtons = [
         Label: document.getElementById('toggle3'),
         Button: document.getElementById('button3'),
         condicao: true
-
     }
 ]
 
@@ -33,14 +46,12 @@ function valida(validacao) {
         esconder(validacao, validacao.condicao);
 
 
-
     } else {
         validacao.condicao = true
         validacao.Label.style.backgroundColor = "#900";
         validacao.Button.style.left = '3px';
         esconder(validacao, validacao.condicao);
     }
-
 }
 
 arrayButtons.forEach(element => {
@@ -50,63 +61,51 @@ arrayButtons.forEach(element => {
 })
 
 function esconder(botao, condicao) {
-    if (botao === arrayButtons[0]) {
-        condicao === false ? map.removeLayer(featureGroup) : featureGroup.addTo(map);
+    if (botao === arrayButtons[0]) {//Testando se o botao que foi ativado foi o botao de esconder
+        condicao === false ? map.removeLayer(featureGroup) : featureGroup.addTo(map);//dependendo da condição, os markers se escondem ou aparece na tela
     }
 }
-var icones = retornarIcon()
+
+
+
+
+map.on('click', e => { adicionar(e); })//evento que ocorre quando o mapa sofre um click
 
 function adicionar(e) {
-    if (!arrayButtons[1].condicao) {
-        var marker = L.marker(e.latlng, { icon: icones.marker }).on('mousemove', function (e) {
-                    e.target.setIcon(icones.markerGrande);
-                }).on('mouseout', function (e) {
-                    e.target.setIcon(icones.marker);
-                });
-        featureGroup.addLayer(marker)
+    if (!arrayButtons[1].condicao) {//Testa se o botao de adicionar está ativado ou desativado
+        var marker = L.marker(e.latlng, { icon: icones.marker })//criando os markers que o usuário adicionou
+            .on('mousemove', e => { e.target.setIcon(icones.markerGrande); })//evento que quando o mouse passa por cima do marker ele fica maior
+            .on('mouseout', e => { e.target.setIcon(icones.marker); });//evento que quando tira o mouse de cima o marker fica menor
+        featureGroup.addLayer(marker);
     }
 }
+
 function apagar(e) {
-    if (!arrayButtons[2].condicao) {
+    if (!arrayButtons[2].condicao) {//teste se o botao de apagar está ativado
         featureGroup.removeLayer(e.layer)
     }
 }
-
-var map = L.map('map').setView([-3.7197137254680808, -38.529790341854095], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    id: "mapbox.streets"
-}).addTo(map);
 
 var markers = [];
 
 if (positionMarkers) {
     positionMarkers.forEach(cord => {
-        var marker = L.marker([cord.latitude, cord.longitude], { icon: icones.shieldPequeno })
-            .on('mousemove', function (e) {
-                e.target.setIcon(icones.shieldGrand);
-            })
-            .on('mouseout', function (e) {
-                e.target.setIcon(icones.shieldPequeno);
-            });
+        var marker = L.marker([cord.latitude, cord.longitude], { icon: icones.shieldPequeno })//criando os markers que vieram do endpoint
+            .on('mousemove', e => { e.target.setIcon(icones.shieldGrand); })//evento que quando o mouse passa por cima do marker ele fica maior
+            .on('mouseout', e => { e.target.setIcon(icones.shieldPequeno); });//evento que quando tira o mouse de cima o marker fica menor
         markers.push(marker)
     });
 }
-var featureGroup = L.featureGroup(markers).on('click', e => {
-    apagar(e);
-}).addTo(map)
 
-map.on('click', function (e) {
-    adicionar(e);
-})
+var featureGroup = L.featureGroup(markers)
+    .on('click', e => { apagar(e); })//caso um dos markers tenha sido clickado ele ativa a função apagar()
+    .addTo(map)//adicionando todos os markers ao mapa
 
-map.on("moveend", function (e) {
+//evento para mostrar a latitude e longitude no quadrinho em baixo do mapa
+map.on("moveend", e => {
     $('#lat-long').val(map.getCenter().lat.toFixed(3) + ' , ' + map.getCenter().lng.toFixed(3));
-});
+});//utilizo JQuery para facilitar na criação do evento
 
-map.fitBounds(featureGroup.getBounds(), {
-    padding: [5, 5]
-});
+//função que quando o mapa é iniciado ele ajusta a posição para um local que possa visualizar todos os markers
+map.fitBounds(featureGroup.getBounds(), { padding: [5, 5] });
 
