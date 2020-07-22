a933148e6b1051b161d4e424b41d3b36e3e4326b
Caio Pessoa <caiopessoa1298@gmail.com>
2020-07-22 03:55:00 -0300

    Criando um arquivo js que será programado todos os objetos referentes aos ícones

---
+++ Funcoes/iconesMarker.js
@@ -0,0 +1,35 @@
+function retornarIcon() {
+    const icones = {
+        shieldPequeno : L.icon({
+            iconUrl: 'Imagens/shield.svg',
+            iconSize: [32, 37],
+            iconAnchor: [16, 37],//metade de 32 e 37
+            popupAnchor: [0, -30]
+
+        }),
+        shieldGrand: L.icon({
+            iconUrl: 'Imagens/shield.svg',
+            iconSize: [32, 37],
+            iconAnchor: [16, 37],//metade de 32 e 37
+            popupAnchor: [0, -30]
+
+        }),
+        marker : L.icon({
+            iconUrl: 'Imagens/marker.svg',
+            iconSize: [32, 37],
+            iconAnchor: [16, 37],//metade de 32 e 37
+            popupAnchor: [0, -30]
+
+        }),
+        markerGrande : L.icon({
+            iconUrl: 'imagens/marker.svg',
+            iconSize: [42, 47],
+            iconAnchor: [16, 37],
+            popupAnchor: [0, -30]
+        })
+    }
+
+
+    return icones;
+}
+
